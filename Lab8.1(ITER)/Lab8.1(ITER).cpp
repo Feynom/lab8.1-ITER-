bb9540dbb862f3be180b6097bd21ab5ad41ac564
Василь <@FEYNOM>
2020-11-22 22:37:37 +0200

    1

--- Lab8.1(ITER)/Lab8.1(ITER).cpp
+++ Lab8.1(ITER)/Lab8.1(ITER).cpp
@@ -6,8 +6,8 @@
 using namespace std;
 
 int kilkist(char* str);
-void modified(char* str);
-void print(char* str);
+char* modified(char* str);
+void print(const char* x);
 
 int main()
 {
@@ -15,14 +15,14 @@ int main()
 	SetConsoleOutputCP(1251);
 
 	char str[201];
+	char second[250];
 
 	cout << "Введіть строку: ";
 	cin.getline(str, 200); cout << endl;
 
 	cout << "Число входжень в послідовність групи букв 'while': " << kilkist(str) << endl;
 	cout << "Модифікована строка: ";
-	modified(str);
-	print(str);
+	print(modified(str));
 
 
 	return 0;
@@ -33,41 +33,52 @@ int kilkist(char* str)
 		return 0;
 	int k = 0;
 	for (int i = 0; i < strlen(str); i++)
-		if (str[i] == 'w')
-			if (str[i + 1] == 'h')
-				if (str[i + 2] == 'i')
-					if (str[i + 3] == 'l')
-						if (str[i + 4] == 'e')
-							k++;
+		if (str[i] == 'w' &&
+			str[i + 1] == 'h' &&
+			str[i + 2] == 'i' &&
+			str[i + 3] == 'l' &&
+			str[i + 4] == 'e')
+			k++;
 	return k;
 }
-void modified(char* str)
+char* modified(char* str)
 {
 	if (strlen(str) < 5)
-		for (int f = 0; f < strlen(str); f++)
-			cout << str[f];
-	int i;
-	for (i = 0; i < strlen(str); i++)
-		if (str[i] == 'w')
-			if (str[i + 1] == 'h')
-				if (str[i + 2] == 'i')
-					if (str[i + 3] == 'l')
-						if (str[i + 4] == 'e')
-						{
-							str[i] = '*';
-							str[i + 1] = '*';
-							str[i + 2] = '*';
-							i += 3;
-							int f = i;
-							for (f; f < strlen(str); f++)
-								str[i] = str[i + 2];
-							i += 2;
-						}
+		return str;
+
+	char* tmp = new char[strlen(str) * 3 / 5 + 1];
+	char* t = tmp;
+
+	tmp[0] = '\0';
+	int i = 0;
+	while (str[i] != 0)
+	{
+		if (str[i] == 'w' &&
+			str[i + 1] == 'h' &&
+			str[i + 2] == 'i' &&
+			str[i + 3] == 'l' &&
+			str[i + 4] == 'e')
+		{
+			strcat(t, "***");
+			t += 3;
+			i += 5;
+		}
+		else
+		{
+			*t++ = str[i++];
+			*t = '\0';
+		}
+	}
+	*t++ = str[i++];
+	*t++ = str[i++];
+	*t = '\0';
+	strcpy(str, tmp);
+	return tmp;
 }
-void print(char* str)
+void print(const char* x)
 {
-	//if(kilkist(str) != 0)
-		for (int i = 0; i < strlen(str); i++)
-			cout << str[i];
+	int i = 0;
+	while(x[i] != '\0')
+		cout << x[i++];
 }
 
